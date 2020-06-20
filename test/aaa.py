b4efb193c2714efe6eb1813c85018ff616f977d6
xuhshen <xuhshen@163.com>
2020-06-20 16:13:27 +0800

    initial

---
+++ test/aaa.py
@@ -0,0 +1,43 @@
+# Python有五个标准的数据类型：
+# 
+# Numbers（数字）
+# String（字符串）
+# List（列表）
+# Tuple（元组）
+# Dictionary（字典）
+
+counter = 100 # 赋值整型变量
+miles = 1000.0 # 浮点型
+name = "John" # 字符串
+
+# a = ["a","2","3"] 
+# a.append("4")
+# a.append(["4","5","6"]) #追加一个元素
+# a.extend(["4"] ) #追加多个元素
+# 
+# print(a)
+# 
+# a = a[:-1]  #重新赋值，不要最后一个元素
+# 
+# print(a)
+# 
+# print("把列表里索引为6的元素 删除：") 
+# a.pop(6)  #把列表里索引为6的元素 删除
+# 
+# print(a)
+
+# print(a[0:3:3])
+
+# c = [1] #列表
+# b = (1,) #元组
+# 
+# print(b[:1])
+
+
+d = {"a":123}
+if d:
+    print(11)
+else:print(22)
+
+
+
